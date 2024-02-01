b15fe5db084a3df954dda661f25f39d24de61ed3
Timur Sultanov <tssultanov@outlook.com>
2024-02-02 01:19:51 +0400

    Optimize VADDPS and VCVTSS2SD

--- Instructions/VADDPS.h
+++ Instructions/VADDPS.h
@@ -5,6 +5,11 @@ public:
     VADDPS(const xed_decoded_inst_t *xedd) : CompilableInstruction(xedd) {}
 private:
     void implementation(bool upper, bool compile_inline) {
-        addps(operands[0].toEncoderOperand(upper), operands[1].toEncoderOperand(upper));
+        if (operands[0].reg() == operands[1].reg()) {
+            addps(operands[0].toEncoderOperand(upper), operands[2].toEncoderOperand(upper));
+        } else {
+            movups(operands[0].toEncoderOperand(upper), operands[1].toEncoderOperand(upper));
+            addps(operands[0].toEncoderOperand(upper), operands[2].toEncoderOperand(upper));
+        }
     }
 };
--- Instructions/VCVTSS2SD.h
+++ Instructions/VCVTSS2SD.h
@@ -5,7 +5,11 @@ public:
     VCVTSS2SD(const xed_decoded_inst_t *xedd) : CompilableInstruction(xedd) {}
 private:
     void implementation(bool upper, bool compile_inline) {
-        movups(operands[0].toEncoderOperand(upper), operands[1].toEncoderOperand(upper));
-        cvtss2sd(operands[0].toEncoderOperand(upper), operands[2].toEncoderOperand(upper));
+        if (operands[0].reg() == operands[1].reg()) {
+            cvtss2sd(operands[0].toEncoderOperand(upper), operands[2].toEncoderOperand(upper));
+        } else {
+            movups(operands[0].toEncoderOperand(upper), operands[1].toEncoderOperand(upper));
+            cvtss2sd(operands[0].toEncoderOperand(upper), operands[2].toEncoderOperand(upper));
+        }
     }
 };
