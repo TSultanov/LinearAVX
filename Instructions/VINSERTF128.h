3400b3dd362691495b9a20b452ebe063a1f3fc6a
Timur Sultanov <tssultanov@outlook.com>
2024-02-02 00:50:07 +0400

    VINSERTF128 fix

--- Instructions/VINSERTF128.h
+++ Instructions/VINSERTF128.h
@@ -5,17 +5,15 @@ public:
     VINSERTF128(const xed_decoded_inst_t *xedd) : CompilableInstruction(xedd) {}
 private:
     void implementation(bool upper, bool compile_inline) {
+        movups(operands[0].toEncoderOperand(upper), operands[1].toEncoderOperand(upper));
+
         auto value = operands[3].imm8Value();
         if(value) {
             if (upper) {
                 movups(operands[0].toEncoderOperand(upper), operands[2].toEncoderOperand(upper));
-            } else {
-                movups(operands[0].toEncoderOperand(upper), operands[1].toEncoderOperand(upper));
             }
         } else {
-            if (upper) {
-                movups(operands[0].toEncoderOperand(upper), operands[1].toEncoderOperand(upper));
-            } else {
+            if (!upper) {
                 movups(operands[0].toEncoderOperand(upper), operands[2].toEncoderOperand(upper));
             }
         }
