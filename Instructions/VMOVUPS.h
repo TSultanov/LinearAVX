2e784bc3454e347d8c009bbdde65fb3e0403e68c
Timur Sultanov <tssultanov@outlook.com>
2024-02-05 21:49:23 +0400

    Fix VMOVUPS

--- Instructions/VMOVUPS.h
+++ Instructions/VMOVUPS.h
@@ -6,5 +6,9 @@ public:
 private:
     void implementation(bool upper, bool compile_inline, ymm_t *ymm) {
         movups(operands[0].toEncoderOperand(upper), operands[1].toEncoderOperand(upper));
+
+        if (operands[0].isXmm()) {
+            zeroupperInternal(ymm, operands[0]);
+        }
     }
 };
